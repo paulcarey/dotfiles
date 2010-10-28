4a114149e5cf9154096dce4b877e6e20aa778504
Paul Carey <paul.p.carey@gmail.com>
2010-10-28 18:48:29 +0100

    Tweaks

--- unpack.sh
+++ unpack.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# Run from the dotfiles directory
+
 ln -s ~/dotfiles/.vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim/gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.vim/ ~/.vim
 
-# cd ~/dotfiles/.vim
-# git submodule update --init
+git submodule update --init
 
 # For Command-T
-# cd ~/.vim/ruby/command-t
-# ruby extconf.rb
-# make
+# cd ~/.vim/bundle/command-t
+# rvm use system or use_leopard_ruby
+# rake make
