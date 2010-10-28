1e93315afebc5100940b27817da99033a2a6fc85
Paul Carey <paul.p.carey@gmail.com>
2010-10-28 18:17:22 +0100

    Added Vim bundles

---
+++ unpack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim/gvimrc ~/.gvimrc
+
+# cd ~/dotfiles/.vim
+# git submodule update --init
+
+# For Command-T
+# cd ~/.vim/ruby/command-t
+# ruby extconf.rb
+# make
