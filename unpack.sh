f03aff2eff382682e93e9fc5f8c6e06124330e18
Paul Carey <paul.p.carey@gmail.com>
2011-02-19 09:27:50 +0000

    Readline and clojure changes

--- unpack.sh
+++ unpack.sh
@@ -5,7 +5,9 @@
 ln -s ~/dotfiles/.vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim/gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.vim/ ~/.vim
+
 ln -s ~/dotfiles/.editrc ~/.editrc
+ln -s ~/dotfiles/.inputrc ~/.inputrc
 
 git submodule update --init
 
