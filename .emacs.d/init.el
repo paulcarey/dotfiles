352fad7b406151a13dce18f2ef9358a4a0072ad5
Paul Carey <paul.p.carey@gmail.com>
2012-04-01 11:22:05 +0100

    Update emacs config with keybindings and theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,3 +20,17 @@
 
 (setq inferior-lisp-program "lein-cljs-repl")
 
+;; Set up Solarized
+(add-to-list 'custom-theme-load-path
+             "/Users/paulcarey/.emacs.d/paulcarey/emacs-color-theme-solarized")
+
+(load-theme 'solarized-light t)
+
+;; Set the default font if not in a terminal
+(if window-system
+    (set-face-attribute 'default nil :font "Inconsolata-15"))
+
+;; Disable the right meta key
+;; Also see http://stackoverflow.com/questions/6344389/osx-emacs-unbind-just-the-right-alt
+(setq mac-option-key-is-meta t)
+(setq mac-right-option-modifier nil)
