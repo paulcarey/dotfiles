0a9e23a41d452a0ff7a2346a75da4e9129c9e9d6
Paul Carey <paul.p.carey@gmail.com>
2012-10-17 12:56:21 +0100

    Add nrepl to emacs init.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,7 @@
 ;; Add in your own as you wish:
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings
                                   clojure-mode clojure-test-mode
-                                  markdown-mode)
+                                  markdown-mode nrepl)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -57,7 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1056c413dd792adddc4dec20e8c9cf1907e164ae" "b0950b032aa3c8faab4864ae288296dd66b92eca" default))))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1056c413dd792adddc4dec20e8c9cf1907e164ae" "b0950b032aa3c8faab4864ae288296dd66b92eca" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -69,7 +69,6 @@
 (setq auto-mode-alist
           (cons '("\\.ml[iyl]?$" .  caml-mode) auto-mode-alist))
 
-;; you should probably comment the next line or replace ~remy by another path 
 (setq load-path (cons "/Users/paulcarey/.emacs.d/paulcarey/ocaml-emacs" load-path))
 
 (autoload 'caml-mode "ocaml" (interactive)
