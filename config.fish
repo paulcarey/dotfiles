20616651a18bece17c005c576934a547fa080431
Paul Carey <paul.p.carey@gmail.com>
2013-12-20 16:44:51 +0000

    Updates for fish, tmux and vim.

---
+++ config.fish
@@ -0,0 +1,48 @@
+function cdtr
+  cd ~/dev/projects/totalrecallr
+end
+
+function gst
+  git status
+end
+
+function gd
+  git diff
+end
+
+# Find file
+function ff
+  ack -f | ack -i $argv
+end
+
+# Find file, ignorning env
+function fff
+  ack --noenv -f | ack -i $argv
+end
+
+function mv
+  command mv -i $argv
+end
+
+function man
+  vim -u ~/.qvimrc -c ":map q :q<CR>" -c ":Man $argv" -c ":only | :set ts=8"
+end
+
+function tree
+  command tree -C $argv
+end
+
+function ber
+  bundle exec rails $argv
+end
+
+function bek
+  bundle exec rake $argv
+end
+
+function cfe
+  coffee -bps $argv
+end
+
+set PATH /usr/local/bin ~/bin /usr/bin /bin /usr/sbin /sbin
+
