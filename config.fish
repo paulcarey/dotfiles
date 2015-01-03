91165ce921b2d05b30003bd717f929f8a7107639
Paul Carey <paul.p.carey@gmail.com>
2015-01-03 23:20:34 +0000

    Minor updates.

--- config.fish
+++ config.fish
@@ -47,3 +47,34 @@ end
 status --is-interactive; and . (rbenv init -|psub)
 
 set PATH (rbenv root)/shims /usr/local/bin ~/bin /usr/bin /bin /usr/sbin /sbin
+set PATH /usr/bin /bin /usr/sbin /sbin
+set PATH /usr/local/bin ~/bin $PATH
+set PATH (rbenv root)/shims $PATH
+set PATH /Applications/Postgres93.app/Contents/MacOS/bin $PATH
+set PATH ~/dev/runtimes/scala-2.10.3/bin/ $PATH
+
+set -x EDITOR /usr/local/bin/vim
+
+function fish_title
+  if test $FISH_TITLE
+    echo $FISH_TITLE
+  else
+    pwd
+  end
+end
+
+function sft
+  set -g -x FISH_TITLE $argv
+end
+
+function transcript
+  ruby -e 'File.readlines(ARGV[0]).each_with_index { |l, i| puts l if i % 4 == 2 }' $argv
+end
+
+function transcripts
+  for fn in (ls *.srt)
+    echo $fn
+    set target_fn (ruby -e 'puts ARGV[0].split(/\./).reverse.drop(1).reverse.join(".") + ".txt"' $fn)
+    transcript $fn > $target_fn
+  end
+end
